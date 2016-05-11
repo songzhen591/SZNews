6cb8a8a1e434f16e53fccab43ffbf23ee70ded21
SongZhen <591555331@qq.com>
2016-05-11 15:52:38 +0800

    first commit

---
+++ SZNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SZNews
+//
+//  Created by 又土又木 on 16/5/11.
+//  Copyright © 2016年 ytuymu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
